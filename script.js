04f6175c896fb1d2d27a4789ea03e50ed10afca6
alexnader-lipatov <lap1993.12@yandex.ru>
2024-01-24 18:37:23 +0300

    3

--- script.js
+++ script.js
@@ -1,53 +1,56 @@
-//  const bal = +prompt('введи бал по экзамену')
+// const arr = [1, 2, 3, 4, 5, 6, 999]
+// function sumArr(arr) {
+//     let res = 0
+//     for (let index = 0; index < arr.length; index++) {
+//         res = res + arr[index]
+//     }
+//     return res
+// }
+// console.log(sumArr(arr))
 
-//  if(bal>=60){
-//     alert("Вы сдали")
-//  }else{
-//     alert("Вы не сдали")
-//  }
 
-// const age = +prompt('Введи возраст')
-// if (age < 18) {
-//     alert('Вы несовершеннолетний')
-// } else if (age >= 65) {
-//     console.log('Вы пожилой человек');
-// } else {
-//     console.log('Вы взрослый');
+// const arr = ['qwe', 'asd', 'qwerty']
+// function lengthArrStr(arr) {
+//     const newArr = []
+//     for (let index = 0; index < arr.length; index++) {
+//         const element = arr[index];
+//         newArr.push(element.length)
+//     }
+//     console.log(newArr);
+// }
+// lengthArrStr(arr)
+
+
+// let i = 1
+// while(i<=10){
+//     if (i % 2 == 0){
+//         console.log(i);
+//     }
+//     i++
 // }
 
 
-// const cofe = confirm('Хочешь кофе?')
-// const tea = confirm('Хочешь чай?')
-// const juice = confirm('Хочешь сок?')
+// const text = document.getElementById('text')
+// text.addEventListener('mouseover', ()=>{
+//     text.style.fontSize = '50px'
+// })
+// text.addEventListener('mouseout', ()=>{
+//     text.style.fontSize = '30px'
+// })
 
-// const drink = prompt('выбери напиток')
 
 
-// if (drink === "кофе") {
-//     alert("Пользователь выбрал кофе.")
-// } else if (drink === "чай") {
-//     alert("Пользователь выбрал чай.")
-// } else if (drink === "сок") {
-//     alert("Пользователь выбрал сок.")
-// } else {
-//     alert("Неверный выбор напитка.")
+// const arr = [5,3,5,6,78,9,0,4,1,2,3,4,3]
+// function maxNumber(arr){
+//     let maxNum = 0
+//     for (let index = 0; index < arr.length; index++) {
+//         const element = arr[index];
+//         if (element > maxNum) {
+//             maxNum = element
+//         }
+        
+//     }
+//     return maxNum
 // }
-
-
-
-// const distance = +prompt('расстояние до места назначения')
-
-// if (distance < 5) {
-//     alert("Подойдет пройти пешком.")
-// } else if (distance >= 5 && distance <= 20) {
-//     alert("Сообразитесь взять велосипед.")
-// } else if (distance > 20) {
-//     alert("Вам нужен автобус.")
-// }
-
-
-// const n1 = +prompt('num1')
-// const n2 = +prompt('num2')
-
-// alert(n1 > n2 ? `Число num1 больше` : n1 < n2 ?`Число num2 больше`: n1===n2 ? `Числа равны`: `неверный ввод`)
-
+// const res = maxNumber(arr)
+// console.log(res);
